aa002cdddbdafa6c1af79d41ed68dadbda04151f
Jorge Rodríguez de la Vega <j.delavega@ingeniusolutions.com>
2023-09-19 22:13:42 +0200

    update main.ipynb to account for all datasets

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
-from imblearn.over_sampling import SMOTE
-
-OVERSAMPLING_TECHNIQUE = SMOTE
 # moodle dataset
 MOODLE_DATA_PATH = "data/base/moodle_numerico.csv"
 MOODLE_IGNORED_FEAT = ["course"]
+MOODLE_IGNORED_LABELS = []
 MOODLE_TARGET = "mark"
 # kaggle dataset
 KAGGLE_DATA_PATH = "data/kaggle/dataset.csv"
+KAGGLE_IGNORED_FEAT = []
 KAGGLE_IGNORED_LABELS = ["Enrolled"]
 KAGGLE_TARGET = "Target"
 # student prediction dataset
 STUDENT_PRED_PATH = "data/student_grade_prediction/train_validate/csv/none.csv"
+STUDENT_PRED_IGNORED_FEAT = []
+STUDENT_PRED_IGNORED_LABELS = []
 STUDENT_PRED_TARGET = "label (fail=1, pass=0)"
